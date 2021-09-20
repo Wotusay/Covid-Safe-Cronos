0e0cb9d905abaf188230ee1d862cb78e61d598b9
wotusay <wout.salembier@hotmail.com>
2021-09-20 13:05:01 +0200

    added firebase config

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,23 @@
+import * as firebase from 'firebase/app';
+
 import SolidStore from './SolidStore';
 
 class RootStore {
   solidStore: any;
+  firebase: any;
 
   constructor() {
+    const firebaseConfig = {
+      apiKey: process.env.REACT_APP_apiKey,
+      authDomain: process.env.REACT_APP_authDomain,
+      projectId: process.env.REACT_APP_projectId,
+      storageBucket: process.env.REACT_APP_storageBucket,
+      messagingSenderId: process.env.REACT_APP_messagingSenderId,
+      appId: process.env.REACT_APP_appId,
+    };
     // eslint-disable-next-line @typescript-eslint/no-unused-expressions
     this.solidStore = new SolidStore(this);
+    this.firebase = firebase.initializeApp(firebaseConfig);
   }
 }
 
